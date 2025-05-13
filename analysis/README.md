b9fa4dcd5b2e73a077303aa12371aa7513ea4832
James A. Fellows Yates <jfy133@gmail.com>
2025-05-13 15:25:54 +0200

    Remove old files

--- analysis/README.md
+++
@@ -1,3 +0,0 @@
-# Analysis directory
-
-This directory will contain the final results of the nf-core/mag run, once executed as per the main [tutorial](https://github.com/paleobiotechnology/nfcore-mag-adna-protocol/blob/main/README.md).
--- analysis/data/AncientMetagenomeDir_curl_download_script.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR357/005/ERR3579735/ERR3579735_2.fastq.gz -o ERR3579735_2.fastq.gz
-curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR357/005/ERR3579735/ERR3579735_1.fastq.gz -o ERR3579735_1.fastq.gz
