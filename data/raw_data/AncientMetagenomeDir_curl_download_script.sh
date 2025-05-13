97410ee47ac1faa1c8d6ca64de716b7b0945bf95
James A. Fellows Yates <jfy133@gmail.com>
2025-05-13 15:29:11 +0200

    Add raw data AMDirT helper files

---
+++ data/raw_data/AncientMetagenomeDir_curl_download_script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR101/049/ERR10114849/ERR10114849_2.fastq.gz -o ERR10114849_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR101/049/ERR10114849/ERR10114849_1.fastq.gz -o ERR10114849_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR357/001/ERR3579731/ERR3579731_2.fastq.gz -o ERR3579731_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR357/002/ERR3579732/ERR3579732_2.fastq.gz -o ERR3579732_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR357/002/ERR3579732/ERR3579732_1.fastq.gz -o ERR3579732_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR357/001/ERR3579731/ERR3579731_1.fastq.gz -o ERR3579731_1.fastq.gz
